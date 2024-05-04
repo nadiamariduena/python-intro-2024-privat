fb16166d3ae5da8d18cd81f89ce734d939dd7820
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-04 18:06:51 -0500

    conca

--- LESSON_01/concatenation.py
+++ LESSON_01/concatenation.py
@@ -3,3 +3,6 @@ last = "Blaz"
 ##its like in js, it concatenate the 2 variables into 1
 fullname = first + " " + last
 print(fullname)
+# in addition you can also add this below
+fullname += "!"
+print(fullname)
