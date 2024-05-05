8783db2b41e0e416c2ece77f8ee043e279068bd0
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-04 22:12:36 -0500

    multiline

---
+++ LESSON_01/multiline.py
@@ -0,0 +1,14 @@
+multiline = '''
+Hey, how are you?
+
+I was just checking in.
+                         All good things
+'''
+print(multiline)
+
+#
+#
+# Escaping special characters
+ #
+sentence = 'I\'m back at work!\tHey!\n\nWhere\'s this at \\located'
+print(sentence)
