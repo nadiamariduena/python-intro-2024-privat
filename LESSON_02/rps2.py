8011ee978832899e583f2340ebbb29d53d968134
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-07 12:35:38 -0500

    rock papaer scissors

---
+++ LESSON_02/rps2.py
@@ -0,0 +1,14 @@
+import sys
+
+import random
+
+print("")
+
+playerchoice = input("Enter... \n1 for Rock, \n2 for Paper, or \n3 for Scissors: \n\n")
+
+print("")
+
+player = int(playerchoice)
+
+if player < 1 | player > 3:
+    sys.exit("You must enter 1,2 or 3 'not below 1 or more than 3'")
