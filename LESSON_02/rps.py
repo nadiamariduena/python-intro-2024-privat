aaa156d5eea538761d2d075d2c97ef7a3150d217
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-06 21:42:54 -0500

    rock papaer scissors

--- LESSON_02/rps.py
+++ LESSON_02/rps.py
@@ -1,3 +1,6 @@
+import sys
+#  without the sys you cannot EXIT the program on line 15
+#
 print("")
 
 playerChoice = input("Enter...\n1 for Rock, \n2 for Paper, or \n3 for Scissors:\n\n")
@@ -6,5 +9,7 @@ playerChoice = input("Enter...\n1 for Rock, \n2 for Paper, or \n3 for Scissors:\
 #
 # print(playerChoice)
 if playerChoice < 1  | playerChoice > 3:
-    print("You ust enter 1, 2, or 3")
+    # print("You ust enter 1, 2, or 3")
     # sys.exit("You must enter 1, 2 , or 3")
+     # TO EXIT the program
+    sys.exit("You must enter 1, 2 , or 3")
