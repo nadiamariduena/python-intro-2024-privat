9179abbe32436caa48e5ff652bab9d494e9513eb
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-06 23:08:00 -0500

    rock papaer scissors

--- LESSON_02/rps.py
+++ LESSON_02/rps.py
@@ -40,6 +40,8 @@ elif player == 2 and computer == 1:
     print("You win!")
 elif player == 3 and computer == 2:
     print("You win!")
+elif player == computer:
+    print("Tie game!")
 else:
     print("Python wins")
 
