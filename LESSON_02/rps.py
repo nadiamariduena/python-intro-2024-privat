4f89a96bc476cc7e22e766a09f6ce8629150db66
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-06 21:33:46 -0500

    rock papaer scissors

--- LESSON_02/rps.py
+++ LESSON_02/rps.py
@@ -6,4 +6,5 @@ playerChoice = input("Enter...\n1 for Rock, \n2 for Paper, or \n3 for Scissors:\
 #
 # print(playerChoice)
 if playerChoice < 1  | playerChoice > 3:
-    sys.exit("You must enter 1, 2 , or 3")
+    print("You ust enter 1, 2, or 3")
+    # sys.exit("You must enter 1, 2 , or 3")
