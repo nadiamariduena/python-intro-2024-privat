f6cb5a203c45a305a0a8cab284436380b4746040
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-11 15:07:44 -0500

    dictionaries

--- LESSON_04/dictionaries.py
+++ LESSON_04/dictionaries.py
@@ -1,6 +1,19 @@
+# version 1
+band ={
+
+"vocals": "Plant",
+"guitar": "Page"
+
+}
+print(band)
+#result: {'vocals': 'Plant', 'guitar': 'Page'}
+#
+# version 2
+
 band2 = dict(
 vocals="Plant",
 guitar="Page"
 )
 
 print(band2)
+#result: {'vocals': 'Plant', 'guitar': 'Page'}
