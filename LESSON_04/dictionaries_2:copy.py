727412e47e50674cf61fceff172ff812a208dec7
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-11 23:44:53 -0500

    dictionaries

--- LESSON_04/dictionaries_2:copy.py
+++ LESSON_04/dictionaries_2:copy.py
@@ -4,12 +4,17 @@ animalss ={
 "anim1": "parrot",
 "anim2": "eagle"
 }
-
+fishes ={
+"anim1": "dorada",
+"anim2": "espadon"
+}
 print(animalss)
+print(fishes)
 #result
 # {'anim1': 'parrot', 'anim2': 'eagle'}
+# {'anim1': 'dorada', 'anim2': 'espadon'}
 #
 #
 # ---- copy -------
-# it s not a good thing to create a copy in such way, because here we are not creating a copy but a reference, and it means that they are both refereing to the same place in memory
-animalss = animalss # create a reference
+# it s not a good thing to create a copy in such way, because here we are not creating a copy but a reference, and it means that they are both refereeing to the same place in memory or the same dictionary, so if i add or remove somthing to animalss , the fishes will be affected
+animalss = fishes # create a reference
