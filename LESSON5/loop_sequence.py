f83819d8116dee667abc1d891d10be883327651a
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-14 21:39:02 -0500

    loops

--- LESSON5/loop_sequence.py
+++ LESSON5/loop_sequence.py
@@ -7,3 +7,17 @@ for i in range(5):
 # 2
 # 3
 # 4
+
+playlist = ["Shape of You", "Despacito", "Uptown Funk", "See You Again", "Closer"]
+
+print("My Spotify Playlist:")
+for i in range(len(playlist)):
+    print(f"{i+1}. {playlist[i]}")
+# RESULT:
+# My Spotify Playlist:
+#1. Shape of You
+# 2. Despacito
+# 3. Uptown Funk
+# 4. See You Again
+# 5. Closer
+#
