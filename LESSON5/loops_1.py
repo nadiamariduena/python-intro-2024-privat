7b82191ef0004f9a1bdad4ea334574a62f92ae9b
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-14 19:01:48 -0500

    loops

--- LESSON5/loops_1.py
+++ LESSON5/loops_1.py
@@ -8,9 +8,22 @@ value = 1
 while value < 10:
     print(value)
     value += 1
-
+#result
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# ------ **
+print('---- example 2 ----')
+#------- **
 #
-#-------
+#
+#
 # Another WAY to break the LOOP
 #--------
 value = 1
@@ -19,3 +32,9 @@ while value <= 10:
     if value == 5:
         break #if the condition is met, it will stop the loop at 5
     value += 1
+    #result
+# 1
+# 2
+# 3
+# 4
+# 5
