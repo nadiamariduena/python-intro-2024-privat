762c4af01dfe554e0237d6b61b0a66571553be7c
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-15 23:20:42 -0500

    loops

--- LESSON5/loop_sequence-2__.py
+++ LESSON5/loop_sequence-2__.py
@@ -186,3 +186,27 @@ for xmore in range(5, 101, 5):
     print(xmore)
 else:
     print('Glad that\'s over!')
+
+    # result:
+
+#     5
+# 10
+# 15
+# 20
+# 25
+# 30
+# 35
+# 40
+# 45
+# 50
+# 55
+# 60
+# 65
+# 70
+# 75
+# 80
+# 85
+# 90
+# 95
+# 100
+# Glad that's over!
