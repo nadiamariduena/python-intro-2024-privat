003919f01c7fb29f7e65e0e5f7da87b83502b18d
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-24 14:44:30 -0500

    rps

--- LESSON_6_Functions/funct_01.py
+++ LESSON_6_Functions/funct_01.py
@@ -1,6 +1,10 @@
+
+
 def hello():
    print('Hello World 🍰')
 #
 #
 # just like in js, you call the function like here below: name + parentheses
 hello()
+## you can only use underscores but not minuses, capital or  uppercase, only lowercase
+# hello_world()
