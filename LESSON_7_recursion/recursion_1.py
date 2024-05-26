153dc5073823a6bd93dbb005ae7718490ce641bc
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-26 07:35:55 -0500

    recursion functions

---
+++ LESSON_7_recursion/recursion_1.py
@@ -0,0 +1,21 @@
+# check the explanation on 17 recursion
+def add_one(num):
+    if (num >= 9):
+        return num + 1
+
+    total = num + 1
+    print(total)
+
+    return add_one(total)
+
+add_one(0)
+
+#1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
