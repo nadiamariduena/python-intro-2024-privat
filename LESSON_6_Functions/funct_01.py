1f4abe41f0cdb1c8adc427f226ec8bbd70426ac6
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-25 19:05:04 -0500

    functions

--- LESSON_6_Functions/funct_01.py
+++ LESSON_6_Functions/funct_01.py
@@ -78,3 +78,20 @@ multiple_items("Dave", "John", "Noaln")
 #result
 #('Dave', 'John', 'Noaln')
 # <class 'tuple'>
+#
+#
+#
+# EXAMPLE 2
+
+def myFun(arg1, *argv):
+    print('First argument :', arg1)
+    for arg in argv:
+        print('Next argument through *argv :', arg)
+
+myFun('Hello', 'Welcome', 'to', 'Paradisse')
+# RESULT:
+# First argument : Hello
+# Next argument through *argv : Welcome
+# Next argument through *argv : to
+# Next argument through *argv : Paradisse
+
