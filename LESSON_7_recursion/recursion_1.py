068e096096476ed7a8875abdfcc3b0d699daf8fe
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-27 09:40:42 -0500

    recursion functions

--- LESSON_7_recursion/recursion_1.py
+++ LESSON_7_recursion/recursion_1.py
@@ -43,15 +43,27 @@ print(mynewtotal)
 print('---')
 # ------
 
-value = "y"
-count = 0
+# value = "y"
+# count = 0
 
+# while value:
+#     count += 1
+#     print(count)
+#     if (count == 5):
+#        break
+#     else:
+#         value = 0
+#         continue
+# # result:1
+# SO, from the moment the loop starts, the value is no longer 0 and therefore is no longer true, but false, so iT adds 1 and the total is 1
+
+value = True  # Start with value set to True
+count = 0     # Initialize count to 0
+
+# Begin the loop
 while value:
-    count += 1
-    print(count)
-    if (count == 5):
-       break
-    else:
-        value = 0
-        continue
-# result:1
+    count += 1       # Increase count by 1
+    print(count)     # Print the current count
+
+    if count == 5:   # Check if count is 5
+        break        # If count is 5, stop the loop
