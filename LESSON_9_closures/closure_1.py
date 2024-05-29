92f4fb57adb1cefdbc94a85200df4a53a58b2704
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-28 20:06:38 -0500

    closures

--- LESSON_9_closures/closure_1.py
+++ LESSON_9_closures/closure_1.py
@@ -1,3 +1,19 @@
 # Closure is a function having accesss to the scope of its parent.
 #
 # Function after the parent function has returned
+
+##  ----
+# COINS example
+# -------
+
+#
+#
+def parent_function(person):
+    coins = 3
+
+    def play_game():
+        nonlocal coins
+        coins -= 1
+
+        if coins > 1:
+            print("")
