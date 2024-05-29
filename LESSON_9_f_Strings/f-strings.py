cd6acb2d67c5eb141c844b2252c75c47b00d3ea1
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-28 22:00:35 -0500

    closures

--- LESSON_9_f_Strings/f-strings.py
+++ LESSON_9_f_Strings/f-strings.py
@@ -8,5 +8,6 @@ print("\n" + person + " has " + str(coins) + " coins left." )
 # Lets find another way of creating the same string
 message = "\n%s hass %s coins left." % (person, coins)
 print(message)
-#
+# RESULT
+# Dave hass 3 coins left.
 #
