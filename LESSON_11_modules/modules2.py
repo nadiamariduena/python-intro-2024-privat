114c635499784b31a0f49d8bb0bf2b98f971a799
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-31 07:02:25 -0500

    python modules

--- LESSON_11_modules/modules2.py
+++ LESSON_11_modules/modules2.py
@@ -13,7 +13,8 @@ def randomfunfact():
     funfacts = [
         "Kansas is a considered flat, but it does have a mount.",
         "Wichita is the largest city in the state, but many would guess that it is Kansas City",
-
+        "Aconsiderable portion of Kansas City is actually in Missouri",
+        "Most Kansans are annoyed by Wisard of Oz references from people outside of Kansas"
     ]
 
     index = choice('0123')
