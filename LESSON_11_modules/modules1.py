183ce80e455a4442fc4d3c3424a26934302d2321
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-31 07:20:09 -0500

    python modules

--- LESSON_11_modules/modules1.py
+++ LESSON_11_modules/modules1.py
@@ -1,5 +1,7 @@
 
-# -----🤚 import the module kansas from the modules2.py
+# -----🤚 import the module kansas from the modules_2_kansas.py
+# if the name of the file is modules_2_kansas and you import it here below as only kansas or modules2 etc, it will not work.
+# the name of the file is the one you will be using to import it here, like sere below
 import modules_2_kansas
 
 
