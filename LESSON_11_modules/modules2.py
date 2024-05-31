d8c5342f92bfe010b5dc47b9fb51855a52bd97e7
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-05-31 07:00:30 -0500

    python modules

--- LESSON_11_modules/modules2.py
+++ LESSON_11_modules/modules2.py
@@ -10,4 +10,12 @@ song = "Home on the Range"
 
 
 def randomfunfact():
+    funfacts = [
+        "Kansas is a considered flat, but it does have a mount.",
+        "Wichita is the largest city in the state, but many would guess that it is Kansas City",
 
+    ]
+
+    index = choice('0123')
+
+    print(funfacts[int(index)])
