d8a91112d80583de60805f253516d616d8b1ed8e
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-01 21:48:59 -0500

    Command Line Arguments

--- LESSON_12_CommandLineArgs/argsParse2.py
+++ LESSON_12_CommandLineArgs/argsParse2.py
@@ -8,4 +8,7 @@ def hello(name, lang):
     #
     msg = f"{greetings[lang]} {name}"
     # In Python, square brackets [] are used to access elements from a dictionary using their keys. In this code, greetings is a dictionary where each language (key) is associated with its respective greeting (value).
+    #
+    #So, when you write greetings[lang], Python looks up the value associated with the key lang in the greetings dictionary. The lang variable contains the language passed to the function hello(), and it's used as the key to retrieve the appropriate greeting from the greetings dictionary.
+    #
     print(msg)
