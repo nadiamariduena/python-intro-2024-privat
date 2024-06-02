2f1e04fd35d6550f52de5f6d6d569ef6e67c4a08
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-01 23:44:50 -0500

    Command Line Arguments

--- LESSON_12_CommandLineArgs/argsParse3_spotif.py
+++ LESSON_12_CommandLineArgs/argsParse3_spotif.py
@@ -1,22 +1,8 @@
-def spotify_greeting(username, lang):
-    music_preferences = {
-        "pop": {
-            "English": "Hey there, pop lover",
-            "Spanish": "!Hola amante del pop",
-            "German": "Hallo, Pop-Liebhaber"
-            },
-        "rock": {
-            "English": "Rock on, {}",
-            "Spanish": "!Rockea, {}",
-            "German": "Rocke weiter, {}"
-
-            },
-        "jazz": {
-            "English": "Smooth vibes coming your way, {}",
-            "Spanish": "!Vibra suave, {}",
-            "German": "Sanfte Vibes für dich, {}"
+def recommend_song(mood, genre):
+    recommendations ={
+        "happy": {
+            "pop": "Happy by Pharrell Williams",
+            "rock": "Don't Stop Believin' by Journey",
+            "hip hop": "Can't Stop the Feeling! by Justin Timberlake"
         }
     }
-
-    # Simulating fetching user's music preferences from Spotify API
-    # Let's assume we have a function called fetch_music_preferences(username) that returns user's music preferences
