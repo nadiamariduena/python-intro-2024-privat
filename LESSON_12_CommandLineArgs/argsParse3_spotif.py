c40ce397a0788ea503f15b72a0c09d399a29b5de
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-01 23:21:35 -0500

    Command Line Arguments

--- LESSON_12_CommandLineArgs/argsParse3_spotif.py
+++ LESSON_12_CommandLineArgs/argsParse3_spotif.py
@@ -1,6 +1,10 @@
 def spotify_greeting(username, lang):
     music_preferences = {
-        "pop": {},
+        "pop": {
+            "English": "Hey there, pop lover",
+            "Spanish": "!Hola amante del pop",
+            "German": "Hallo, Pop-Liebhaber"
+            },
         "rock": {},
         "jazz": {}
     }
