e49e3245ba59af4a791cf7bef5bf9212a91b2897
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-01 23:45:48 -0500

    Command Line Arguments

--- LESSON_12_CommandLineArgs/argsParse3_spotif.py
+++ LESSON_12_CommandLineArgs/argsParse3_spotif.py
@@ -3,6 +3,11 @@ def recommend_song(mood, genre):
         "happy": {
             "pop": "Happy by Pharrell Williams",
             "rock": "Don't Stop Believin' by Journey",
-            "hip hop": "Can't Stop the Feeling! by Justin Timberlake"
+            "hip hop": "Can't Stop the Feeling! by Justin    Timberlake"
+        },
+        "chill": {
+            "pop": "Sunflower by Post Malone & Swae Lee",
+            "electronic": "Strobe by Deadmau5",
+            "indie": "Holocene by Bon Iver"
         }
     }
