1e8f13b08a65bee4bf72aacafe812ac77ea20b97
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-02 15:29:22 -0500

    Command Line Arguments

--- LESSON_12_CommandLineArgs/argsParse3_banking.py
+++ LESSON_12_CommandLineArgs/argsParse3_banking.py
@@ -3,5 +3,7 @@ def send_money(amount, currency, recipient):
     exchange_rates = {
         #GBP) is the currency of the United Kingdom
         "USD": {"EUR": 0.82, "GBP": 0.72},
+        "EUR": {"USD": 1.22, "GBP": 0.88},
+        "GBP": {"USD": 1.39, "EUR": 1.14}
 
     }
