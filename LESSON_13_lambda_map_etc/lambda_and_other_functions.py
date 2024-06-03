2fe1b201128ef23b6f066a0bdc2c234e7f44acd4
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-02 21:34:59 -0500

    lambda functions

--- LESSON_13_lambda_map_etc/lambda_and_other_functions.py
+++ LESSON_13_lambda_map_etc/lambda_and_other_functions.py
@@ -1,8 +1,9 @@
-# def squared(num): return num + num
-# print(squared(2))
+def squared(num): return num + num
+print(squared(2))
 #result
 #4 // num + num or 2 + 2 = 4
 
 #🖐️ here you have to be careful, if you dont add parenthesis to the lambda like here (lambda num : num * num), it will reformat it, and you wont get the result 4
-squared = (lambda num : num * num)
+squared = (lambda num1 : num1 * num1)
 print(squared(2))
+# the reformat happens in line 8, when typing (squared(2))
