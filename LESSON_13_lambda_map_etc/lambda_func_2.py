b55f6b4123c762bddebad0b53d3fcadfc670bf60
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-04 10:33:04 -0500

    lambda functions

--- LESSON_13_lambda_map_etc/lambda_func_2.py
+++ LESSON_13_lambda_map_etc/lambda_func_2.py
@@ -1,3 +1,4 @@
+# notice what is happening here
 def funcBuilder(x):
     return lambda num: num + x
 
@@ -6,3 +7,7 @@ addTwenty = funcBuilder(20)
 
 print(addTen(7))
 print(addTwenty(7))
+
+# result
+#17
+# 27
