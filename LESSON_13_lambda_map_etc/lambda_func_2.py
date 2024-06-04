34e4e2ca474372f79ea3468a2ade97a3f040dfa2
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-04 15:54:54 -0500

    lambda functions

--- LESSON_13_lambda_map_etc/lambda_func_2.py
+++ LESSON_13_lambda_map_etc/lambda_func_2.py
@@ -11,3 +11,22 @@ print(addTwenty(7))
 # result
 #17
 # 27
+def playlistBuilder(songs):
+    return lambda playlist_name: f"Adding songs to {playlist_name} playlist: {', '.join(songs)}"
+
+# List of songs for the "Chill Vibes" playlist
+chill_vibes_songs = ["Song 1", "Song 2", "Song 3"]
+# List of songs for the "Summer Hits" playlist
+summer_hits_songs = ["Song A", "Song B", "Song C"]
+
+
+# here you are assigning the value of chill_vibes_songs to addChillVibesSongs, then the addChillVibesSongs will be appending a new song "Song 4"
+addChillVibesSongs = playlistBuilder(chill_vibes_songs)
+addSummerHitsSongs = playlistBuilder(summer_hits_songs)
+
+# Add a new song to the "Chill Vibes" playlist
+chill_vibes_songs.append("Song 4")
+
+print(addChillVibesSongs("Chill Vibessss"))
+print(addSummerHitsSongs("Summer Hits"))
+
