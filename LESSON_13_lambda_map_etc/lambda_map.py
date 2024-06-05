af4335330a120a30d7ee7a059f94c2935bd516a9
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-05 16:46:20 -0500

    lambda functions

--- LESSON_13_lambda_map_etc/lambda_map.py
+++ LESSON_13_lambda_map_etc/lambda_map.py
@@ -1,6 +1,7 @@
 strings = [ "apple", "banana", "cherry", "date", "elderberry"]
 
 reversed_strings = list(map(lambda s: s[::-1], strings))
+#[::-1] is a slicing notation used to reverse a sequence (string, list, tuple, etc.).
 
 print(reversed_strings)
 #RESULT
