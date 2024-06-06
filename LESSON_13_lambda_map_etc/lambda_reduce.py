5275e35786178326a0162b105acb7eb32c2dcee0
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-06 00:04:55 -0500

    lambda functions

--- LESSON_13_lambda_map_etc/lambda_reduce.py
+++ LESSON_13_lambda_map_etc/lambda_reduce.py
@@ -108,3 +108,6 @@ total_revenue = reduce(lambda acc, sale: acc + sale[1], shoe_sales, 0)
 # In this example, the lambda function lambda acc, sale: acc + sale[1] takes two arguments: acc (the accumulated revenue so far) and sale (the current sale tuple). It extracts the price (the second element) from each sale tuple and adds it to the accumulated revenue. The reduce function applies this lambda function cumulatively to the list shoe_sales, starting with an initial value of 0 for the accumulated revenue, resulting in the total revenue generated from selling shoes.
 
 print("Total revenue from selling shoes:", total_revenue, "dollars")
+
+# result
+# Total revenue from selling shoes: 350 dollars
