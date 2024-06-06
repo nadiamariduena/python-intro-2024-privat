656b30262434ff20501944a910e07433c8781176
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-05 23:07:05 -0500

    lambda functions

--- LESSON_13_lambda_map_etc/lambda_reduce.py
+++ LESSON_13_lambda_map_etc/lambda_reduce.py
@@ -13,3 +13,8 @@ print(total)
 # result
 # 16
 # it s doing an addition of 1 + 2 + 3 + 4 + 5 +1
+# THE BELOW is the equivalent but without the rreduce
+def sum_total(a,b): return  a + b
+print(sum_total(10,19))
+#result
+#29
