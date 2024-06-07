facc9baff64c64378f4618f1c7b86d29b4b55796
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-07 00:05:36 -0500

    lambda functions

--- LESSON_13_lambda_map_etc/lambda_reduce.py
+++ LESSON_13_lambda_map_etc/lambda_reduce.py
@@ -170,28 +170,4 @@ print("Total revenue from orders:", total_revenue, "dollars")
 
 # Total revenue from orders: 125 dollars
 
-#
-#
-# ** pizza resto 2
-# ------------
-# include a payment service that accepts different types of cards to handle the total of the order. We'll define a dictionary mapping card types to their respective processing fees, and then we'll calculate the total revenue after deducting the processing fees.
-
-
-#
-def calculate_total_revenue_2(orders__3):
-
-    if not orders__3: #Base case: If there are no order, return 0
-        return 0
-    else:
-        # Extract the total price of the current order and add it to
-        # the revenue
-        return orders__3[0][2] + calculate_total_revenue_2(orders__3[1:])
-
-orders__3 = [
-     ("Alice", ["Pizza", "Salad"], 25),
-    ("Bob", ["Burger", "Fries", "Soda"], 20),
-    ("Charlie", ["Pizza", "Wings", "Soda"], 30),
-    ("David", ["Salad", "Soup"], 15),
-    ("Eve", ["Pizza", "Burger", "Wings"], 35)
-]
 
