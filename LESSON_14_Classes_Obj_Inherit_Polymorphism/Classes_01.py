062c3cf8f665ba77b9ed00063c6c5a0c167a3929
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-09 12:03:04 -0500

    classes

--- LESSON_14_Classes_Obj_Inherit_Polymorphism/Classes_01.py
+++ LESSON_14_Classes_Obj_Inherit_Polymorphism/Classes_01.py
@@ -6,4 +6,11 @@
 class Pizza:
     # Constructor method to initialize the pizza with toppings and size
     #
+    #
     def __init__(self):
+    # - The self parameter is a reference to the current instance of the class, and is used to access variables that belongs to the class.
+    # - It does not have to be named self , you can call it whatever you like (rainbow, potato but keep it clear), but it has to be the first parameter of any function in the class:
+
+
+
+
