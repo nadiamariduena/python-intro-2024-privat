2df32207c3dafb4c5b89561aebdea5b171ad5451
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-09 13:08:06 -0500

    classes

--- LESSON_14_classes/classes_01.py
+++ LESSON_14_classes/classes_01.py
@@ -6,7 +6,9 @@
 class Pizza:
     # Constructor method to initialize the pizza with toppings and size
     #
-    #
+    # ** __new__ Method in Python Defined
+    # In Python, __new__ is a static method that’s responsible for creating and returning a new instance of the class. It takes the class as its first argument followed by additional arguments.
+
     def __init__(self, toppings, size):
     # - The self parameter is a reference to the current instance of the class, and is used to access variables that belongs to the class.
     # - It does not have to be named self , you can call it whatever you like (rainbow, potato but keep it clear), but it has to be the first parameter of any function in the class:
@@ -15,7 +17,7 @@ class Pizza:
       self.size = size # properties: size
 
 
-# 🧑‍🍳 Method to bake the pizza
+# 🧑‍🍳  Method to bake the pizza
 def bake(self):
     print(f"Baking a {self.size}-inch pizza with {', '.join(self.toppings)} toppings!")
 
