1bf30cdc5ef6cee270d51235eb950f3789f19053
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-10 14:47:18 -0500

    getter and setter

--- LESSON_14_classes/classes_0.py
+++ LESSON_14_classes/classes_0.py
@@ -4,21 +4,32 @@
 class Vehicle:
 
     #In Python, __init__ is an instance method that initializes a newly created object. It takes the object as its first argument followed by additional arguments.
-    def __init__(self, make, model):
+    def __init__(self, make, model, place):
         self.make = make
         self.model = model
+        self.place = place
 
     def moves(self):
+         print("MOves along...")
+
+    def get_make_model(self):
         # print("MOves along...")
         print(f"John drives a {self.make} '{self.model}'") # output: John drives a Tesla 'Model 3'
         #or  print(f"MOves along...{self.model}")
 
+    def get_car_to_place(self):
+        # print("MOves along...")
+        print(f"on direction to {self.place}")
 
-my_car = Vehicle("Tesla", "Model 3")
+
+
+
+my_car = Vehicle("Tesla", "Model 3", "Oregon")
 
 #
 print(my_car.make)
 print(my_car.model)
+print(my_car.place)
 
 #
 my_car.moves()
@@ -30,4 +41,15 @@ my_car.moves()
 
 
 # 2) example
-# ADDING a getter
+# lets create more cars from the same class from above
+
+my_car.get_make_model()
+my_car.moves()
+my_car.get_car_to_place()
+###
+#
+print("---------")
+#
+your_car = Vehicle("Cadillac", "Escalada", "Paris")
+your_car.get_make_model()
+your_car.moves()
