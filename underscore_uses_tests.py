97f5d2c88c82440f81b8a49a974795c4f8f5a642
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-10 16:13:05 -0500

    underscore USES on python

---
+++ underscore_uses_tests.py
@@ -0,0 +1,10 @@
+class Pizza:
+        #
+        # ---------------
+    def __init__(self, toppings):
+        self.toppings = toppings
+        #
+        #
+        self.slices = 8
+        self.slices_eaten = 0
+        #-----------------
