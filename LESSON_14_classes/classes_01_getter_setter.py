b911a060eb41d68c9e7cee497170819f06c11bb1
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-09 21:14:28 -0500

    getter and setter

--- LESSON_14_classes/classes_01_getter_setter.py
+++ LESSON_14_classes/classes_01_getter_setter.py
@@ -2,3 +2,8 @@
 class Circle:
     def __init__(self, radius):
         self.radius = radius # Private attribute
+
+    # GETTER method for radius (property)
+    # property
+    def radius(self):
+        return self._radius
