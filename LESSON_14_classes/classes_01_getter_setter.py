a327a4e744dbe121126f4f40d7787a8bc6faa98b
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-09 21:19:28 -0500

    getter and setter

--- LESSON_14_classes/classes_01_getter_setter.py
+++ LESSON_14_classes/classes_01_getter_setter.py
@@ -4,11 +4,13 @@ class Circle:
         self.radius = radius # Private attribute
 
     # GETTER method for radius (property)
+    # you need to declare both @property and @radius.setter decorators if you want to use both the getter and setter methods within a single property in Python.
     @property
     def radius(self):
         return self._radius
 
     # Setter method from radius property
+    # you need to declare both @property and @radius.setter decorators if you want to use both the getter and setter methods within a single property in Python.
     @radius.setter
     def radius(self, value):
         if value > 0:
