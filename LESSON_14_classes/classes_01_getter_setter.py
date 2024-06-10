58290f008e12bc2ae2d372214ac271ed0e433a6c
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-10 15:15:25 -0500

    getter and setter

--- LESSON_14_classes/classes_01_getter_setter.py
+++ LESSON_14_classes/classes_01_getter_setter.py
@@ -1,74 +1,30 @@
-## GETTER & SETTER (the first example will be boring asf, next one will be interesting)
-class Circle:
-    def __init__(self, radius):
-        self.radius = radius # Private attribute
+class SampleClass:
 
-    # GETTER method for radius (property)
-    # ** you need to declare both @property and @radius.setter decorators if you want to use both the getter and setter methods within a single property in Python.
-    @property
-    def radius(self):
-        return self._radius
+    def __init__(self, a):
+        ## private varibale or property in Python
+        self.__a = a
 
-    # Setter method from radius property
-    # ** you need to declare both @property and @radius.setter decorators if you want to use both the getter and setter methods within a single property in Python.
-    @radius.setter
-    def radius(self, value):
-        if value > 0:
-            self._radius = value
-        else:
-            print("Invalid radius, Please provide a positive value.")
+    ## getter method to get the properties using an object
+    def get_a(self):
+        return self.__a
 
-# Create an instance of Circle
-my_circle = Circle(5)
+    ## setter method to change the value 'a' using an object
+    def set_a(self, a):
+        self.__a = a
 
 
-# Get the radius (via property)
-print("Circle radius (via property):", my_circle.radius)
-
-my_circle.radius = 10
-print("Circle radius (after setting via property):", my_circle.radius)
 
 
+## creating an object
+# assign the 10 value to the obj (or whatever variable you want)
+# obj = SampleClass(10)
+sweetpotato = SampleClass(10)
 #
 #
-#-----------
-# CAR example
-# -----------
-#
-#
-class Car:
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
+## getting the value of 'a' using get_a() method
+print(sweetpotato.get_a())
 
-    # Getter method for year attribute
-    def get_year(self):
-        return self.year
+## setting a new value to the 'a' using set_a() method
+sweetpotato.set_a(45)
 
-    # Setter method for year attribute with validation
-    def set_year(self, year):
-        if isinstance(year, int) and year > 0:
-            self.year = year
-        else:
-            print("Invalid year value. Please provide a positive integer.")
-
-# Create an instance of the Car class
-my_car = Car("Toyota", "Camry", 2015)
-
-# Accessing the year using the getter method
-print("Car year (via getter):", my_car.get_year())
-
-# Trying to set an invalid year using the setter method
-my_car.set_year("Invalid")
-print("Car year (after invalid set):", my_car.get_year())
-
-# Setting a valid year using the setter method
-my_car.set_year(2020)
-print("Car year (after valid set):", my_car.get_year())
-
-# result
-# Car year (via getter): 2015
-# Invalid year value. Please provide a positive integer.
-# Car year (after invalid set): 2015
-# Car year (after valid set): 2020
+print(sweetpotato.get_a())
--- LESSON_14_classes/classes_01_getter_setter_2.py
+++ LESSON_14_classes/classes_01_getter_setter_2.py
@@ -1,1 +1,82 @@
+## GETTER & SETTER (the first example will be boring asf, next one will be interesting)
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius # Private attribute
+
+    # GETTER method for radius (property)
+    # ** you need to declare both @property and @radius.setter decorators if you want to use both the getter and setter methods within a single property in Python.
+    @property
+    def radius(self):
+        return self._radius
+
+    # Setter method from radius property
+    # ** you need to declare both @property and @radius.setter decorators if you want to use both the getter and setter methods within a single property in Python.
+    @radius.setter
+    def radius(self, value):
+        if value > 0:
+            self._radius = value
+        else:
+            print("Invalid radius, Please provide a positive value.")
+
+# Create an instance of Circle
+my_circle = Circle(5)
+
+
+# Get the radius (via property)
+print("Circle radius (via property):", my_circle.radius)
+
+my_circle.radius = 10
+print("Circle radius (after setting via property):", my_circle.radius)
+
+
+#
+#
+#-----------
+# CAR example
+# -----------
+#
+#
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    # Getter method for year attribute
+    def get_year(self):
+        return self.year
+
+    # Setter method for year attribute with validation
+    def set_year(self, year):
+        if isinstance(year, int) and year > 0:
+            self.year = year
+        else:
+            print("Invalid year value. Please provide a positive integer.")
+
+# Create an instance of the Car class
+my_car = Car("Toyota", "Camry", 2015)
+# Accessing the year using the getter method
+print("Car year (via getter):", my_car.get_year())
+# Trying to set an invalid year using the setter method
+my_car.set_year("Invalid")
+print("Car year (after invalid set):", my_car.get_year())
+
+# Setting a valid year using the setter method
+my_car.set_year(2020)
+print("Car year (after valid set):", my_car.get_year())
+
+# result
+# Car year (via getter): 2015
+# Invalid year value. Please provide a positive integer.
+# Car year (after invalid set): 2015
+# Car year (after valid set): 2020
+
+
+
+
+
+
+
+
+
 # managing orders at a pizza restaurant, including pizzas with various toppings, drinks, and calculating the total bill with an optional tip for the server. We'll use properties with getters and setters to control access to the order items and calculate the total bill:
