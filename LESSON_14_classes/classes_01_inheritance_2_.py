a42e87eed48c9084ec2e8898bce455c3e959927d
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-12 08:49:44 -0500

    classes | object, INHERITANCE

--- LESSON_14_classes/classes_01_inheritance_2_.py
+++ LESSON_14_classes/classes_01_inheritance_2_.py
@@ -1,12 +1,16 @@
 class IceCreamStore:
 
 
+    # 1
     def __init__(self):
         """Initialize the
         IceCreamSTore with an empty list of toppings and a default
         extra topping COST
         """
-
+        # ** 2
+        self.toppings = []
+        self.extra_topping_cost = 0.80 # The cost for extra toppings, because who doesn't love some extra toppings?
+    # 3
     def add_topping(self, topping, extra=False):
 
         """
@@ -20,7 +24,10 @@ class IceCreamStore:
         - extra (bool, optional): Whether the topping is extra. Defaults to FALSE
 
         """
-
+        # 4
         if extra:
-            self.append((topping, self.extra_topping_cost))  # If extra, append with extra cost
+            self.toppings.append((topping, self.extra_topping_cost))  # If extra, append with extra cost
+        else:
+
+            self.toppings.append((topping, 0)) # Otherwise, no extra cost
 
