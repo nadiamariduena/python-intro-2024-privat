c78cbea6bec4d2e873a9f943900982d7199902e8
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-12 08:42:36 -0500

    classes | object, INHERITANCE

---
+++ LESSON_14_classes/classes_01_inheritance_2_.py
@@ -0,0 +1,8 @@
+class IceCreamStore:
+
+
+    def __init__(self):
+        """Initialize the
+        IceCreamSTore with an empty list of toppings and a default
+        extra topping COST
+        """
