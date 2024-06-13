727b24e8a56d2e93e1ed0ffed247602524f0b6e0
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-13 18:41:04 -0500

    classes | Polyphormism

--- LESSON_14_classes/classes_01_polyphormism_0.py
+++ LESSON_14_classes/classes_01_polyphormism_0.py
@@ -1,1 +1,5 @@
-# The BIG BOSS pizza class that all the other pizzas look up to
+# The 👾 BIG BOSS pizza class that all the other pizzas look up to
+
+class PizzaBoss:
+    def __init__(self, toppings):
+        self.toppings = toppings
