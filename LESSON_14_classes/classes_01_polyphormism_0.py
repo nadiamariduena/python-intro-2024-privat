06400907e1c5a133ecd9a93934d2ade5a39411ba
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-13 23:35:49 -0500

    classes | Polyphormism

--- LESSON_14_classes/classes_01_polyphormism_0.py
+++ LESSON_14_classes/classes_01_polyphormism_0.py
@@ -1,6 +1,14 @@
 # The big boss account class that all the other accounts look up to
 # 💸 BOSS
 class BankAccountBoss:
+    #
+    #
     def __init__(self, account_number, balance):
         self.account_number = account_number
         self.balance = balance
+
+    # A special method to represent the account when printed
+    def __str__(self):
+        return f"Account Number: {self.account_number}, Balance: ${self.balance}"
+        # The dollar sign was  used as a symbol to represent the currency, not like in react `${somevariable}`
+    #--------------------------------------------
