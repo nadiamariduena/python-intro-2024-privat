f95696d2cb0353ab82df5383a1439f999a7debd3
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-16 22:36:10 -0500

    error  Exception Handling

---
+++ LESSON_15_err-exception-handling/error__exception_list.py
@@ -0,0 +1,3 @@
+# ** Built-in Exceptions
+
+## Beginner-friendly examples of handling Python exceptions based on common exceptions from the list provided by W3Schools: https://www.w3schools.com/python/python_ref_exceptions.asp
