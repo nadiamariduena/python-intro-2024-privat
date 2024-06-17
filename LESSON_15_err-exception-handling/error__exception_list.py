d49186c12bee1816961021b2d234e4a3b7bb40ab
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-16 23:14:01 -0500

    error  Exception Handling

--- LESSON_15_err-exception-handling/error__exception_list.py
+++ LESSON_15_err-exception-handling/error__exception_list.py
@@ -1,3 +1,25 @@
 # ** Built-in Exceptions
 
 ## Beginner-friendly examples of handling Python exceptions based on common exceptions from the list provided by W3Schools: https://www.w3schools.com/python/python_ref_exceptions.asp
+
+#
+# ** ----- Value ERROR -----
+#
+
+try:
+    num = int(input("Enter a number: "))
+    if num < 0:
+        raise ValueError("Number must be positive")
+    print("Number entered:", num)
+
+except ValueError as e:
+    print("Invalid input", e)
+
+# OUTPUT
+# if you type: -20 or using the minus, its going to be negative, and it will give you this:
+# Enter a number: -10
+# Invalid input Number must be positive
+#
+# BUT if you type a positibe number like: 20 , it will give you this:
+# Enter a number: 20
+# Number entered: 20
