341f0ab15bb8aa343972e00f8630e14e124ec758
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-06-17 22:12:51 -0500

    OOP banking example

--- LESSON_16_OOP/BankingIntro/bank_accounts_0.py
+++ LESSON_16_OOP/BankingIntro/bank_accounts_0.py
@@ -1,4 +1,5 @@
 class bank_account:
     def __init__(self, initialAmount, accountName):
+# the balance and name here are attributes that represent initialAmount & accountName respectively
         self.balance = initialAmount
         self.name = accountName
