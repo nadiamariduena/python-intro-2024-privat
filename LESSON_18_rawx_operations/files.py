3c4ab50fe6b1780850f49bca8c4f76b7498e4ecd
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-15 02:12:53 -0500

    RAWX operation, similar to CRUD

---
+++ LESSON_18_rawx_operations/files.py
@@ -0,0 +1,8 @@
+# Acronym for:
+
+# r = Read
+# a = Append
+# w = Write
+# x = Create
+
+# Read - Error if the file doesn't exist
--- z_R-A-W-X.md
+++ z_R-A-W-X.md
@@ -153,4 +153,6 @@ project_folder/
 # a = Append
 # w = Write
 # x = Create
+
+# Read - Error if the file doesn't exist
 ```
