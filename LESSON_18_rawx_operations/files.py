8a4e59adcea7e6910578ee4926ccec07ec191e39
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-15 17:23:26 -0500

    RAWX operations, similar to CRUD

--- LESSON_18_rawx_operations/files.py
+++ LESSON_18_rawx_operations/files.py
@@ -9,7 +9,7 @@
 # f stands for FILE
 # r stands for read, i will use it here below,  as the second argument
 
-# f = open("names.txt", "r") # the r is telling it to read the file, but without the r, it also tells it to read the file, but only when its the r alone
+# f = open("names.txt", "r") # The 'r' indicates that it is reading the file. Without the 'r', it also reads the file, but only when 'r' is the sole parameter.
 
 f = open("names.txt")
 print(f.read())
