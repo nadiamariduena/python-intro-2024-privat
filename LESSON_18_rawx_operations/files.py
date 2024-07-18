13d228aa343291d2accbda266a32b9b4eaf414d9
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-17 23:35:03 -0500

    BYTES: Can bytes be used in Python only with the seek() method, or are there other methods or approaches available?

--- LESSON_18_rawx_operations/files.py
+++ LESSON_18_rawx_operations/files.py
@@ -1,6 +1,81 @@
-data = b'Hello'
-byte_list = list(data)
-bytearray_data = bytearray(data)
+# Acronym for:
 
-print(byte_list)
-print(bytearray_data)
+# r = Read
+# a = Append
+# w = Write
+# x = Create
+
+# Read - Error if the file doesn't exist
+# f stands for FILE
+# r stands for read, i will use it here below,  as the second argument
+
+# f = open("names.txt", "r") # The 'r' indicates that it is reading the file. Without the 'r', it also reads the file, but only when 'r' is the sole parameter.
+
+f = open("names.txt")
+# print(f.read())
+#print(f.read(4)) # it will reach the first 4 character file, which in this case will be Dave
+
+for line in f:
+    print(line)
+
+print("----")
+
+f.close()
+
+try:
+    f = open("names.txt")
+    print(f.read())
+except:
+    print("The file you want to read doesn't exists")
+finally:
+    f.close()
+
+
+#
+print("_____ 👾 ______")
+# ------
+#
+# APPEND 1. ('a') a: stands for APPEND
+# ------
+f = open('names.txt', 'a')
+f.write('David Lee Roth 🥥')
+f.close()
+
+f = open('names.txt')
+print(f.read())
+f.close()
+#
+#
+#
+print("_____ 🦄 ______")
+#
+#
+# ------
+# WRITE ('w') w: stands for WRITE
+# write & Overwrite, we will overwrite what we have, when we will open the txt
+# ------
+
+# f = open("context.txt", "w")
+# f.write('i deleted all of the context.txt content')
+# f.close()
+
+# with open('context.txt', 'w') as f:
+#     f.write('Hello, world!\n')
+#     f.close()
+
+
+# print all lines
+# with open('context.txt', 'r') as file:
+#     lines = file.readlines()
+#     print(lines)
+
+# output:
+# ['# context\n', 'Rainbow\n', 'Horse\n', 'Zebra\n', 'Zonkey\n', 'Donkey']
+
+
+
+with open('context.txt', 'r') as file:
+    file.seek(10)  # Move to the 10th byte
+    contentx = file.read()
+    print(contentx)
+
