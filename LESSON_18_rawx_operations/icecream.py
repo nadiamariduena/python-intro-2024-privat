66a7a4c070223656aefb93880d8e31d4943beda6
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-18 22:22:29 -0500

    FILE Modes

--- LESSON_18_rawx_operations/icecream.py
+++ LESSON_18_rawx_operations/icecream.py
@@ -7,9 +7,12 @@ menu_file = open("menu.txt", "w")
 # 2 Write the menu items to the file
 # Add the stuff below to the generated menu.txt in step 1
 menu_file.write("Menu:\n")
-menu_file.write("1. Chocolate Ice Cream: $3.50\n")
-menu_file.write("2. Vanilla Ice Cream: $3.00\n")
+menu_file.write("1. 🍨 Chocolate Ice Cream: $3.50\n")
+menu_file.write("2. 🍦 Vanilla Ice Cream: $3.00\n")
 
 # after the STEP 1 & 2 run the code, type:  python icecream.py
 
 
+# Close the file to SAVE the changes
+
+menu_file.close()
