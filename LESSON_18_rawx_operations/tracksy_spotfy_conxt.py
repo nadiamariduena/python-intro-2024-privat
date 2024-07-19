cb81357bc73b604af69b10cfc7041b1e2fe425fa
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-18 23:57:56 -0500

    FILE Modes - playlist exercise, managing multiple fles operations/  context

--- LESSON_18_rawx_operations/tracksy_spotfy_conxt.py
+++ LESSON_18_rawx_operations/tracksy_spotfy_conxt.py
@@ -11,4 +11,9 @@ with open("tracks_combined_playlist.txt", "w") as combined_file:
    for filename in filenames:
        # Open each file in read mode
        with open(filename, "r") as file:
+           #Write a header indicating the filename
+           combined_file.write(f"=== {filename} ===\n")
+           #
+           #
+           # Iterate over each line in the current file
 
