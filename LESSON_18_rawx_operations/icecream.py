158cc3d711a50af6a4b945aeabc9091a8615800a
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-18 22:07:11 -0500

    FILE Modes

--- LESSON_18_rawx_operations/icecream.py
+++ LESSON_18_rawx_operations/icecream.py
@@ -1,4 +1,5 @@
 # 🟡 When you use 'w' mode (open('menu.txt', 'w')), it will create the menu.txt file if it doesn't already exist.
+#1 step
 menu_file =("menu.txt", "w")
 
 # Write the menu items to the file
@@ -6,5 +7,6 @@ menu_file.write("Menu:\n")
 menu_file.write("1. Chocolate Ice Cream: $3.50\n")
 menu_file.write("2. Vanilla Ice Cream: $3.00\n")
 
+# after the STEP 1 run the code, type:  python icecream.py
 
 
