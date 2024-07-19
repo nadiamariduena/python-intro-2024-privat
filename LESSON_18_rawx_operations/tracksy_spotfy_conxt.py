f27572840d2a6758f38d9c757d256e3f8d5e4c02
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-18 23:54:49 -0500

    FILE Modes - playlist exercise, managing multiple fles operations/  context

--- LESSON_18_rawx_operations/tracksy_spotfy_conxt.py
+++ LESSON_18_rawx_operations/tracksy_spotfy_conxt.py
@@ -6,3 +6,6 @@ filenames = ['tracks_spot_playlist1.txt', 'tracks_spot_playlist2.txt']
 # Using context manager to manage multiple file operations
 # will generate the tracks_combined_playlist.txt
 with open("tracks_combined_playlist.txt", "w") as combined_file:
+
+   # for filename in filenames: Loop through each filename within the filenames list
+   for filename in filenames:
