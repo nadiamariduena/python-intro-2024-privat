506f0b621cd6c3701224678d394e281144f64ff3
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-19 21:41:23 -0500

    pickle exercise

--- LESSON_18_rawx_operations/test2_pickle_shoe.py
+++ LESSON_18_rawx_operations/test2_pickle_shoe.py
@@ -8,5 +8,14 @@ class Shoe:
         self.size = size
         self.color = color
 
+shoes_collection = [
+    Shoe("Nike", 9, "Black"),
+    Shoe("Adidas", 8, "White"),
+    Shoe("Puma", 7, "Red")
 
+]
 
+# Serialize the shoes collection object to a file
+file_path = "this_will_be_the_generated_shoes_collection.pickle"
+with open(file_path, "wb") as file:
+    pickle.dump(shoes_collection, file)
