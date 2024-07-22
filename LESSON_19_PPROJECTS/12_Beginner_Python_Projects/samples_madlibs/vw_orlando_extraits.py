a4a6d5257d63a2f7dcc4f8019bfffb9bdb92b862
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-22 12:58:40 -0500

    MADLIBS word game

--- LESSON_19_PPROJECTS/12_Beginner_Python_Projects/samples_madlibs/vw_orlando_extraits.py
+++ LESSON_19_PPROJECTS/12_Beginner_Python_Projects/samples_madlibs/vw_orlando_extraits.py
@@ -1,5 +1,21 @@
+def madlib():
+
+
+    noun1 = input("Noun: ") # ancetres
+    noun2 = input("Noun: ") # peches
+    noun3 = input("Noun: ") # meteore
+
+    verb = input("Verb: ") # vouloir
+    verb1 = input("Verb: ") #  mûrissent
+    verb2 = input("Verb: ")  # brûler
+
+    adj1 = input("Adjective: ") # Pourquoi
+    adj2 = input("Adjective: ")  #vain
+    adj3 = input("Adjective: ") # dernier
 
 
 
+    madlib = f"{adj1} donc Orlando avait-il désiré s’élever au-dessus de ses {noun1} ? Il semblait {adj2} , impertinent au {adj3} point de {verb} renchérir sur cette œuvre anonyme, sur le labeur de ces mains disparues. Mieux valait partir inconnu, laissant derrière soi une arche, un cellier, un mur où {verb1} les {noun2} que {verb2} comme un météore {noun3} qui s’évanouit sans poussière."
 
-# Pourquoi donc Orlando avait-il désiré s’élever au-dessus de ses ancêtres ? Il semblait vain, impertinent au dernier point de vouloir renchérir sur cette œuvre anonyme, sur le labeur de ces mains disparues. Mieux valait partir inconnu, laissant derrière soi une arche, un cellier, un mur où mûrissent les pêches que brûler comme un météore qui s’évanouit sans poussière.
+
+    print(madlib)
