43a7a31ed9359ab6849444278bffe7b7bedcb127
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-22 09:12:26 -0500

    MADLIBS word game

---
+++ LESSON_19_PPROJECTS/game_00/game_0.py
@@ -0,0 +1,1 @@
+# Mad Libs is a word game created by Leonard Stern and Roger Price. It consists of one player prompting others for a list of words to substitute for blanks in a story before reading aloud
