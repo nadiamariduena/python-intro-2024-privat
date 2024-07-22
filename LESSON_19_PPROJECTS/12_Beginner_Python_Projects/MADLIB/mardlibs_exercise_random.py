ee8ff84d0e43e6a5b4877ece4872fdbaff6207b1
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-22 13:03:43 -0500

    MADLIBS word game

---
+++ LESSON_19_PPROJECTS/12_Beginner_Python_Projects/MADLIB/mardlibs_exercise_random.py
@@ -0,0 +1,1 @@
+# For this "random" (random will randomize the data) , you need to have min 2 files with the texts, if you dont have the time to use your own texts, copy the data from this repo: https://github.com/kying18/beginner-projects/blob/master/sample_madlibs/hp.py (it s a good practice to do it on your own)
