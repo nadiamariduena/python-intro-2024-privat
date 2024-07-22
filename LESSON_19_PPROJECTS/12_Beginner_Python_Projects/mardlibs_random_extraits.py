36b874c6158cc7a1007d29eb9b41e67f9433e254
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-22 12:54:24 -0500

    MADLIBS word game

--- LESSON_19_PPROJECTS/12_Beginner_Python_Projects/mardlibs_random_extraits.py
+++ LESSON_19_PPROJECTS/12_Beginner_Python_Projects/mardlibs_random_extraits.py
@@ -18,3 +18,6 @@ def madlib():
 
 
     madlib = f"{adj1} donc Orlando avait-il désiré s’élever au-dessus de ses {noun1} ? Il semblait {adj2} , impertinent au {adj3} point de {verb} renchérir sur cette œuvre anonyme, sur le labeur de ces mains disparues. Mieux valait partir inconnu, laissant derrière soi une arche, un cellier, un mur où {verb1} les {noun2} que {verb2} comme un météore {noun3} qui s’évanouit sans poussière."
+
+
+    print(madlib)
