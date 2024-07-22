af4933301f8684aba18585f543a419ed91505b46
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-22 12:48:47 -0500

    MADLIBS word game

--- LESSON_19_PPROJECTS/12_Beginner_Python_Projects/mardlibs_random_extraits.py
+++ LESSON_19_PPROJECTS/12_Beginner_Python_Projects/mardlibs_random_extraits.py
@@ -8,13 +8,13 @@ def madlib():
 
 
     verb = input("Verb: ") # vouloir
-    verb1 = input("Verb: ") #  murissent
-    verb2 = input("Verb: ")  # bruler
+    verb1 = input("Verb: ") #  mûrissent
+    verb2 = input("Verb: ")  # brûler
 
-    adj1 = input("Adjective: ") # pourquoi
+    adj1 = input("Adjective: ") # Pourquoi
     adj2 = input("Adjective: ")  #vain
     adj3 = input("Adjective: ") # dernier
 
 
 
-    madlib = f"Pourquoi donc Orlando avait-il désiré s’élever au-dessus de ses {noun1} ? Il semblait vain, impertinent au dernier point de {verb} renchérir sur cette œuvre anonyme, sur le labeur de ces mains disparues. Mieux valait partir inconnu, laissant derrière soi une arche, un cellier, un mur où mûrissent les {noun2} que brûler comme un météore {noun3} qui s’évanouit sans poussière."
+    madlib = f"{adj1} donc Orlando avait-il désiré s’élever au-dessus de ses {noun1} ? Il semblait {adj2} , impertinent au {adj3} point de {verb} renchérir sur cette œuvre anonyme, sur le labeur de ces mains disparues. Mieux valait partir inconnu, laissant derrière soi une arche, un cellier, un mur où {verb1} les {noun2} que {verb2} comme un météore {noun3} qui s’évanouit sans poussière."
