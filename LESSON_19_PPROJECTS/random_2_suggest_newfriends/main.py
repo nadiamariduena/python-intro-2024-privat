df6d301a9b6383db9f83d4b63dd5e4e761462bea
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-23 20:21:45 -0500

    random, suggest new friends

--- LESSON_19_PPROJECTS/random_2_suggest_newfriends/main.py
+++ LESSON_19_PPROJECTS/random_2_suggest_newfriends/main.py
@@ -2,4 +2,8 @@
 
 
 
+# import the function and the array/list with the users
+from helper import suggest_random_user, users
 
+def main():
+    #Simulating a current user (in real application, this could come from session or login)
