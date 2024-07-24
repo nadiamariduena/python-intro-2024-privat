23d94bf1b948f0b31c147b17c2dc55402d2b07c2
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-23 19:47:55 -0500

    random, suggest new friends

--- LESSON_19_PPROJECTS/random_2_suggest_newfriends/helper.py
+++ LESSON_19_PPROJECTS/random_2_suggest_newfriends/helper.py
@@ -1,3 +1,8 @@
 # Within the helper.py, I will define a function that generates random users or profiles
 
 # This will simulate fetching friends or suggested connections
+
+#
+import random
+
+def suggest_random_user(current_user_id):
