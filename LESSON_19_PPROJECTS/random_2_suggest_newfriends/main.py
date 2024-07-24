3bc759c886aaa9100b16f894e897acc45d1c597f
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-23 20:31:33 -0500

    random, suggest new friends

--- LESSON_19_PPROJECTS/random_2_suggest_newfriends/main.py
+++ LESSON_19_PPROJECTS/random_2_suggest_newfriends/main.py
@@ -12,7 +12,9 @@ def main():
 
 
     # Suggesting 3 random users that the current user may know
-    print(f"Suggested friends for {users[current_user_id - 1]["name"]}")
     # If current_user_id is 1, then users[current_user_id - 1] means we're looking at the first user in the list (because computers start counting from 0, not 1).
-
     # :🟧 If the current user's ID is 2 (Mario), we want to ensure that Mario is excluded from the list of suggested friends. In programming, we typically start counting from 0 for the first element in a list, so users[current_user_id - 1] refers to the user at index current_user_id - 1 in the users list.
+    print(f"Suggested friends for {users[current_user_id - 1]["name"]}")
+    # This syntax (["name"]) is used because users[current_user_id - 1] returns a dictionary (a type of collection in Python), and dictionaries use keys to access their values.
+
+
