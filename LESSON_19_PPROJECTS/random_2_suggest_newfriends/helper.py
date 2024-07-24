e8b2e2b0b1cd867b359179812ae62f5efcf24fd4
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-23 20:00:56 -0500

    random, suggest new friends

--- LESSON_19_PPROJECTS/random_2_suggest_newfriends/helper.py
+++ LESSON_19_PPROJECTS/random_2_suggest_newfriends/helper.py
@@ -7,13 +7,13 @@ import random
 
 users = [
     {"id": 1, "name": "Alice"},
-      {"id": 2, "name": "Antoine"},
+      {"id": 2, "name": "Mario"},
        {"id": 3, "name": "Anastacia"}
 ]
 
 
 
-
+#suggest_random_user(current_user_id): This function takes current_user_id as a parameter and suggests a random user from users excluding the current user. It uses random.choice to select a random user from other_users, which are all users except the current one.
 def suggest_random_user(current_user_id):
     #
     # Exclude the current user from suggestions
