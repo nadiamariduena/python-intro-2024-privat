3100d94ffd4de46025083dffaada8a667879c9bc
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-24 17:42:00 -0500

    RANDOM: ads images

--- LESSON_19_PPROJECTS/random_1_ads_2/helper.py
+++ LESSON_19_PPROJECTS/random_1_ads_2/helper.py
@@ -1,7 +1,15 @@
+import random
+
 def fashion_ads():
 
     the_ads = [
-        {"image": "image1.jpg", "text": "Discover our Summer Collection!"}
+        {"image": "image1.jpg", "text": "Discover our Summer Collection!"},
+        {"image": "image2.jpg", "text": "Explore the Latest Trends in Fashion!"},
+        {"image": "image3.jpg", "text": "Get Ready for Fall with Our New Arrivals!"},
+        {"image": "image4.jpg", "text": "Stay Stylish This Winter Season!"}
 
 
     ]
+
+
+    return random.choice(the_ads)
