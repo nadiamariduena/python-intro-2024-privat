8ae468d11710991980cb8ac7d93dbad92132cfaf
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-23 20:26:04 -0500

    random, suggest new friends

--- LESSON_19_PPROJECTS/random_2_suggest_newfriends/helper.py
+++ LESSON_19_PPROJECTS/random_2_suggest_newfriends/helper.py
@@ -20,9 +20,9 @@ def suggest_random_user(current_user_id):
 
     other_users = [user for user in users if user["id"] != current_user_id]
 
-   # If current_user_id is 1, then users[current_user_id - 1] means we're looking at the first user in the list (because computers start counting from 0, not 1).
 
-    # :🟧 If the current user's ID is 2 (Mario), we want to ensure that Mario is excluded from the list of suggested friends. In programming, we typically start counting from 0 for the first element in a list, so users[current_user_id - 1] refers to the user at index current_user_id - 1 in the users list.
+
+
     suggested_user = random.choice(other_users)
 
     return suggested_user
--- LESSON_19_PPROJECTS/random_2_suggest_newfriends/main.py
+++ LESSON_19_PPROJECTS/random_2_suggest_newfriends/main.py
@@ -7,3 +7,12 @@ from helper import suggest_random_user, users
 
 def main():
     #Simulating a current user (in real application, this could come from session or login)
+    # this current_user_id is coming from the helper TOO
+    current_user_id = 1 # Assuming current user is Alice with user ID 1
+
+
+    # Suggesting 3 random users that the current user may know
+    print(f"Suggested friends for {users[current_user_id - 1]["name"]}")
+    # If current_user_id is 1, then users[current_user_id - 1] means we're looking at the first user in the list (because computers start counting from 0, not 1).
+
+    # :🟧 If the current user's ID is 2 (Mario), we want to ensure that Mario is excluded from the list of suggested friends. In programming, we typically start counting from 0 for the first element in a list, so users[current_user_id - 1] refers to the user at index current_user_id - 1 in the users list.
