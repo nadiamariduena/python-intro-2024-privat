c9c95ff66182bc6622029c54556b99ac8f985681
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-24 18:00:25 -0500

    RANDOM: ads images

---
+++ LESSON_19_PPROJECTS/random_1_ads_2/main.py
@@ -0,0 +1,7 @@
+from helper import fashion_ads
+
+def display_fashion_ads(ad):
+    """Simmulates displaying an ad.
+    """
+    print(f"Showing ad: {ad['text']}")
+    print(f"Image: {ad['image']}")
