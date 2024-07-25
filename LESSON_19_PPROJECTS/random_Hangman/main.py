57b9c5463379c3efdde7d865a4490e847e569548
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-24 23:58:52 -0500

    HANGMAN: exercise using random and conditions

--- LESSON_19_PPROJECTS/random_Hangman/main.py
+++ LESSON_19_PPROJECTS/random_Hangman/main.py
@@ -28,7 +28,14 @@ def get_valid_word():
 
 def hangman():
   word = get_valid_word(words)  # This line gets a valid word to play with
+  #
+  #🟡 Creating Letters from the Word (set(word)):
   word_letters = set(word)  # This line creates a set of letters from the chosen word
+
+
+
+
+  #
   alphabet = set(string.ascii_uppercase)  # This line creates a set of all uppercase letters in the alphabet
 
 #🟡 Making an Alphabet (set(string.ascii_uppercase)):
