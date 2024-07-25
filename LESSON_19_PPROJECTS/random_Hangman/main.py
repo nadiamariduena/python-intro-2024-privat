6ff556717404dc0b5692b1e6acd45e63905c1da7
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-25 00:04:21 -0500

    HANGMAN: exercise using random and conditions

--- LESSON_19_PPROJECTS/random_Hangman/main.py
+++ LESSON_19_PPROJECTS/random_Hangman/main.py
@@ -27,23 +27,23 @@ def get_valid_word():
         # - Once a valid word (one that does not contain underscores or spaces) is found, the function returns this word.
 
 def hangman():
-  word = get_valid_word(words)  # This line gets a valid word to play with
-  #
-  #🟡 Creating Letters from the Word (set(word)):
-  # Now that we have a word, let's look at the letters it's made of.
-  #
-  # - Imagine your word is "CAT".
-  # - We take each letter from "CAT" (C, A, T) and write them down separately.
-  # - This helps us keep track of which letters are in the word.
-  word_letters = set(word)  # This line creates a set of letters from the chosen word
+    word = get_valid_word(words)  # This line gets a valid word to play with
+    #
+    #🟡 Creating Letters from the Word (set(word)):
+    # Now that we have a word, let's look at the letters it's made of.
+    #
+    # - Imagine your word is "CAT".
+    # - We take each letter from "CAT" (C, A, T) and write them down separately.
+    # - This helps us keep track of which letters are in the word.
+    word_letters = set(word)  # This line creates a set of letters from the chosen word
 
 
 
 
-  #
-  alphabet = set(string.ascii_uppercase)  # This line creates a set of all uppercase letters in the alphabet
+    #
+    alphabet = set(string.ascii_uppercase)  # This line creates a set of all uppercase letters in the alphabet
 
-#🟡 Making an Alphabet (set(string.ascii_uppercase)):
-#
-# In our game, we need to guess letters from A to Z
-# - We call this the alphabet. It's like a list of all the letters we can use. So, we write down all these letters (A, B, C, ..., Z) on a piece of paper.
+    #🟡 Making an Alphabet (set(string.ascii_uppercase)):
+    #
+    # In our game, we need to guess letters from A to Z
+    # - We call this the alphabet. It's like a list of all the letters we can use. So, we write down all these letters (A, B, C, ..., Z) on a piece of paper.
