d1ec02c535dc1b0e4937a519b1f0c16c4fdb5420
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-24 23:29:32 -0500

    HANGMAN: exercise using random and conditions

--- LESSON_19_PPROJECTS/random_1_ads_2/helper.py
+++ LESSON_19_PPROJECTS/random_1_ads_2/helper.py
@@ -3,13 +3,12 @@ import random
 def fashion_ads():
 
     the_ads = [
-        {"image": "image1.jpg", "text": "Discover our Summer Collection!"},
-        {"image": "image2.jpg", "text": "Explore the Latest Trends in Fashion!"},
-        {"image": "image3.jpg", "text": "Get Ready for Fall with Our New Arrivals!"},
-        {"image": "image4.jpg", "text": "Stay Stylish This Winter Season!"}
+        {"image": "image1.jpg", "text":  "🌻 Discover our Summer Collection!"},
+        {"image": "image2.jpg", "text": "🩷 Explore the Latest Trends in Fashion!"},
+        {"image": "image3.jpg", "text": "🟩Get Ready for Fall with Our New Arrivals!"},
+        {"image": "image4.jpg", "text": "🟦Stay Stylish This Winter Season!"}
 
 
     ]
-
 
     return random.choice(the_ads)
--- LESSON_19_PPROJECTS/random_1_ads_2/main.py
+++ LESSON_19_PPROJECTS/random_1_ads_2/main.py
@@ -1,19 +1,44 @@
 from helper import fashion_ads
 
-def display_fashion_ads(ad):
-    """Simmulates displaying an ad.
-    """
-    print(f"Showing ad: {ad['text']}")
-    print(f"Image: {ad['image']}")
-    print("____ ☁️ ______ ")
 
-def main():
-    """Main function to run the program.
-    """
+#step 1
+def display_fashion_list(ad):
 
+
+    print(f" Display Text:  {ad['text']}")
+    print(f" Display Image:  {ad['image']}")
+
+    print("__________")
+
+
+# step 2
+def main_loop():
+
+    #3
+    # show 3 items from the list on the helper.py (i have actually 4 items)
     for x in range(3):
-        ad = fashion_ads()
-        display_fashion_ads(ad)
+
+        #4 assign the 'a' var to the list of items 'fashion_ads' coming from the helper.py
+        a = fashion_ads()
+
+        # 5 use the 'A' which is carrying the list of items 'fashion_ads' coming from the helper.py
+        # display the function in step1, add the param 'a' from step 4) which is carrying the list of items 'fashion_ads' coming from the helper.py
+        display_fashion_list(a)
+
+
+
 
 if __name__ == "__main__":
-    main()
+
+    main_loop()
+
+# ------ output ------------
+# lucizor@lucibellsan:~/Documents/0_PYTHON-all/PYTHON-PRIVAT/python-intro-2024-privat/LESSON_19_PPROJECTS/random_1_ads_2$ python main.py
+#  Display Text:  🌻 Discover our Summer Collection!
+#  Display Image:  image1.jpg
+# __________
+#  Display Text:  🌻 Discover our Summer Collection!
+#  Display Image:  image1.jpg
+# __________
+#  Display Text:  🩷 Explore the Latest Trends in Fashion!
+#  Display Image:  image2.jpg
