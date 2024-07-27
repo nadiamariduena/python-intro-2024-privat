2dfc497078583e08f20aeb9fbab92cbc0d04563c
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-26 20:32:35 -0500

    exercise testing

---
+++ LESSON_19_PPROJECTS/random_Hangman/exercize.py
@@ -0,0 +1,74 @@
+#🟡  fill each comment, analyse the comments, dont copy, just think, if you started to javascript, the syntax in python will confuse but at the end is the same logic
+
+#🔴 repeat this 20 times until you get familiar with the syntax
+
+# -----------------------------------
+# intro: imports
+# import the random, string, and file containing the list of words
+
+
+
+# 🟦 0
+# create the function to grab the words from the list
+
+
+
+# 🟦 1.a grab the data coming from the 'words'
+## Pick a random word from the list
+
+
+
+
+    # 1.b loop: add the condition in which you tell it:
+    ## If the word has underscores or spaces...
+
+
+
+        # 1.c # ... Pick a new word
+
+
+
+    # 1.d # Give back the chosen word
+
+
+
+
+#------------
+# 🟧 # create the function to init the hangman game
+
+
+    #1 make the connection to the first function
+
+
+    #2 TAKES the 'word' and turns it into a special list where each letter is only listed
+
+
+    #
+    #3 This line creates a list of all the letters in the alphabet, from A to Z. It’s like having a list of all the letters you can use to guess the secret word.
+
+
+    #
+    # 4 This line starts with an empty list where you can keep track of the letters you’ve already guessed. At first, it’s empty because you haven’t guessed any letters yet.
+
+
+    #
+    #
+    # 5 add var and assign message input to "guess a letter:" , to uppercase()
+
+
+
+    # CONDITION
+
+
+    # 6 Check if the user letter is in the alphabet, and hasn't been used yet (used_letters)
+
+
+
+        # 7 Add the user letter to the set of used letters
+
+
+        ## 8 If the user letter is in the word letters...
+
+
+             # 9 If the user letter exist within the list of word_letters, remove it
+
--- LESSON_19_PPROJECTS/random_Hangman/main.py
+++ LESSON_19_PPROJECTS/random_Hangman/main.py
@@ -5,29 +5,53 @@ import string
 
 #1
 def get_valid_word(words):
+    #
 
+    #
+# 1 grab the data coming from the 'words'
     word = random.choice(words)
+    # loop: add the condition in which you tell it to continue the loop if the users adds either a _ or ' ' (empty space)
     while '_' in word or ' ' in words:
         word = random.choice(words)
-
+    # stop the loop
     return word
 
 
 def hangman():
+    #1 make the connection to the first function
     word = get_valid_word(words)
+
+    #2 TAKES the 'word' and turns it into a special list where each letter is only listed
     word_letters = set(word)
+
+    #
+    #3 This line creates a list of all the letters in the alphabet, from A to Z. It’s like having a list of all the letters you can use to guess the secret word.
     alphabet = set(string.ascii_uppercase)
+
+    #
+    # 4 This line starts with an empty list where you can keep track of the letters you’ve already guessed. At first, it’s empty because you haven’t guessed any letters yet.
     used_letters = set()
 
     #
     #
-    #
+    # 5 add var and assign message input to "guess a letter:" , to uppercase()
     user_letter = input("Guess a letter: ").upper()
 
+
+    # CONDITION
+
+
+    # 6 Check if the user letter is in the alphabet, and hasn't been used yet (used_letters)
     if user_letter in alphabet - used_letters:
+
+
+        # 7 Add the user letter to the set of used letters
         used_letters.add(user_letter)
 
+        ## 8 If the user letter is in the word letters...
         if user_letter in word_letters:
+
+             # 9 If the user letter exist within the list of word_letters, remove it
             word_letters.remove(user_letter)
 
 
@@ -44,3 +68,4 @@ print(user_input)
 
 if __name__ == '__main__':
     hangman()
+
