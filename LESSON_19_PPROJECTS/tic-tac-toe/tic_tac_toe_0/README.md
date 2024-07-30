d3491acdcb2870bbf9463ebe9c981c8290a5817f
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-07-30 01:20:50 -0500

    tic tac toe game

--- LESSON_19_PPROJECTS/tic-tac-toe/tic_tac_toe_0/README.md
+++ LESSON_19_PPROJECTS/tic-tac-toe/tic_tac_toe_0/README.md
@@ -271,5 +271,10 @@ class HumanPlayer(Player):
 ## 🟡 Create the `game.py`
 
 ```python
+class TicTacToe:
+    def __init__(self):
+        self.board = [' ' for _ in range(9)]
 
+        self.current_winner = None # Keep track of winner
 ```
+
--- LESSON_19_PPROJECTS/tic-tac-toe/tic_tac_toe_0/game.py
+++ LESSON_19_PPROJECTS/tic-tac-toe/tic_tac_toe_0/game.py
@@ -1,4 +1,5 @@
 class TicTacToe:
     def __init__(self):
-        self.board = [' ' for _ in range(9)] # we will use a single list to rep 3x3 board
+        self.board = [' ' for _ in range(9)] # This creates a list (like a row of boxes) with 9 empty spaces. Each space will be where we put X or O. Think of it as a 3x3 grid but stored in a single line.
 
+        self.current_winner = None # Keep track of winner
