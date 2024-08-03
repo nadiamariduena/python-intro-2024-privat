43c5da360d9029b0849187655207aae0f741fb5b
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-08-03 17:25:55 -0500

    decorators: classmethod

--- test-file.py
+++ test-file.py
@@ -1,8 +1,16 @@
 class Student:
 # Class attribute to keep track of all students
+    # THE Bellow looks like an array but it not, so dont confuse it with the JS arrays
     students = []
 
     def __init__(self, name, grade):
         self.name = name
         self.grade = grade
         # ADD the new student to the class list
+
+    @classmethod
+    def had_good_grades(cls): #cls: class
+        """Class method to check if any student has good grades (A or B).
+        """
+
+        for student in cls.students: # (the LIST at the top inside the class, line 3)
