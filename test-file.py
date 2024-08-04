78f9ff953aa69b29ea81db56815199058b5b179d
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-08-04 12:47:33 -0500

    decorators

--- test-file.py
+++ test-file.py
@@ -1,1 +1,17 @@
 from abc import ABC
+
+class Base(ABC):
+
+    @abstractmethod
+    def abstract_method(self):
+        pass
+
+
+    @classmethod
+    def class_method(cls):
+        print("This is class METHOD")
+
+    @staticmethod
+    def static_method():
+        print("This is a static method")
+
