d79f775cca3b6aea411dc30cc490d0eae910aad9
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-08-04 22:24:57 -0500

    loop: for loop exercises

--- test-file.py
+++ test-file.py
@@ -1,3 +1,3 @@
 # Starting Value of the slider
 
-slider_value = 50
+slider_value = 50 # Ltes say the slider start at the middle ( think about a sequence of 3 imgs, 0,1,2 , it will start from picture 1)
