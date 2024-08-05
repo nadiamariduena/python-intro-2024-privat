475a3fe4cc231e11a9e2a9c2c8f3a96fb9a1965c
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-08-04 20:02:04 -0500

    decorators

--- test-file.py
+++ test-file.py
@@ -2,3 +2,7 @@ class Toybox():
     def __init__(self, toy):
         self._toy = None
         self.toy = toy
+
+    @property
+    def toy(self):
+        return self._toy # the🔒LOCK shows what toy is inside
