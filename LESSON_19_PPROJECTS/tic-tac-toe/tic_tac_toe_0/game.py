d800ac820954a032af26c8cfebee291de0106e41
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-08-10 21:19:30 -0500

    tic tac toe game- back to the player

--- LESSON_19_PPROJECTS/tic-tac-toe/tic_tac_toe_0/game.py
+++ LESSON_19_PPROJECTS/tic-tac-toe/tic_tac_toe_0/game.py
@@ -150,3 +150,6 @@ def play(game, x_player, o_player, print_game=[True]):
             print('It\'s a tie!')
 
 
+if __name__ == '__main__':
+    x_player = HumanPlayer('X')
+    o_player = RandomComputerPlayer('O')
