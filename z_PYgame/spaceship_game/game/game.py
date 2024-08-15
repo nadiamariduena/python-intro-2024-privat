1a4b0673a5f51a78f773337bcfda019f4d9cc621
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-08-15 15:32:32 -0500

    pygame: FILL, add color to the new surface/shape

--- z_PYgame/spaceship_game/game/game.py
+++ z_PYgame/spaceship_game/game/game.py
@@ -6,6 +6,14 @@ pygame.init()
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
 pygame.quit()
