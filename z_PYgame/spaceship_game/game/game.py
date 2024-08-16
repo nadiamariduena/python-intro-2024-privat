b079ca7e1c3b0a7999aff3b04d520146968f46e4
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-08-15 20:25:24 -0500

    pygame: animating load image

--- z_PYgame/spaceship_game/game/game.py
+++ z_PYgame/spaceship_game/game/game.py
@@ -8,6 +8,9 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
+
+pygame.display.set_caption('Space shooter')
+
 running = True
 
 
