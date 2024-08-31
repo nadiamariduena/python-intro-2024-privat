425466e9a13e7bc6bda0eb27cf01834a727f3a5e
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-08-31 10:07:19 -0500

    rect & frect: anim/ vectors , delta

--- z_PYgame/spaceship_game/game/game.py
+++ z_PYgame/spaceship_game/game/game.py
@@ -1,3 +1,5 @@
+#🟨 with vector
+
 
 import pygame
 import os
@@ -16,60 +18,90 @@ pygame.display.set_caption("Space shooter")
 
 #while loop related
 running = True
-surf = pygame.Surface((100,200))
-# animation related
-x = 100
+#✋ CLOCK:  FPS (frame per second)
+clock = pygame.time.Clock()
+
+
+
 
 # img's path
 image_paths = {
     'player': os.path.join(script_dir, '..', 'images', 'player.png'),
-    'star': os.path.join(script_dir, '..', 'images', 'star.png')
+    'star': os.path.join(script_dir, '..', 'images', 'star.png'),
+    'meteor': os.path.join(script_dir, '..', 'images', 'meteor.png'),
+    'laser': os.path.join(script_dir, '..', 'images', 'laser.png')
+
 }
 
 
 
 player_surf = pygame.image.load(image_paths['player']).convert_alpha()
 
+
+
+
+meteor_surf = pygame.image.load(image_paths['meteor']).convert_alpha()
+laser_surf = pygame.image.load(image_paths['laser']).convert_alpha()
+
 # (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
 # Will pos the plane at the center of the screen/window
 player_rect = player_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+meteor_rect = meteor_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
-
-
+laser_rect = laser_surf.get_frect(bottomleft=(20, WINDOW_HEIGHT - 10))
 
 # start
 star_surf = pygame.image.load(image_paths['star']).convert_alpha()
 # star pos
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
 
-
+# 1. -----  move right to left loop  ---
+# player_direction = 1
+#🤚 VECTOR
+player_direction = pygame.math.Vector2(20, -10)
+#20 X, - 10Y axis
+# print(player_direction)
+# -----  move right to left loop  ---
 
 while running:
+
+
+    clock.tick(60) #✋
+
+
     for event in pygame.event.get():
        if event.type == pygame.QUIT:
             running = False
 
-
-
- # FILL thplayer_rect = player_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))e window with a red color
- # player = pygame.Rect((300, 250, 50, 50))
- # https://pyga.me/docs/ref/pygame.html
-
- # 7 --- DRAW the game ----
- # list of colors: https://pyga.me/docs/ref/color_list.html
     display_surface.fill("lavenderblush2")
-
-
 
 
     for pos in star_positions:
         display_surface.blit(star_surf, pos)
 
-    # x += 0.1
-    if player_rect.right < WINDOW_WIDTH:
-        player_rect.left  += 6
 
+   # player
     display_surface.blit(player_surf, player_rect)
+
+    # meteor
+    display_surface.blit(meteor_surf, meteor_rect)
+    # laser
+    display_surface.blit(laser_surf, laser_rect)
+
+
+    # it will be multiplied by 100px every single frame
+    # player_rect.x += 20
+    #✋
+    #  if player x direction positive +=, then it will be increased +, hence will move to the right.
+    # -------------
+    # player_rect.y -= 10
+    # UP or down anim, depending of the values
+
+    # player_rect.center += (20, -10) # this will give you an error
+
+    # vector
+    player_rect.center += player_direction
+
     pygame.display.update()
 
 
