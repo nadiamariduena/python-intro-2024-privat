43609868a4f31f1e3af670a980f01b851655a1c1
Nadia Mariduena <nadia.mm.mariduena@gmail.com>
2024-09-08 16:07:51 -0500

    spaceshooter: Handling Player Input | Mouse & Keyboard

---
+++ z_PYgame/spaceship_game/game/game.py
@@ -0,0 +1,108 @@
+#🟡 input & key controls (process)
+
+import pygame
+import os
+from random import randint
+
+
+pygame.init()
+script_dir = os.path.dirname(__file__)
+
+
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Space shooter")
+
+
+#while loop related
+running = True
+#✋ CLOCK:  FPS (frame per second)
+clock = pygame.time.Clock()
+
+
+
+
+# img's path
+image_paths = {
+    'player': os.path.join(script_dir, '..', 'images', 'player.png'),
+    'star': os.path.join(script_dir, '..', 'images', 'star.png'),
+    'meteor': os.path.join(script_dir, '..', 'images', 'meteor.png'),
+    'laser': os.path.join(script_dir, '..', 'images', 'laser.png')
+
+}
+
+
+
+player_surf = pygame.image.load(image_paths['player']).convert_alpha()
+
+
+
+
+meteor_surf = pygame.image.load(image_paths['meteor']).convert_alpha()
+laser_surf = pygame.image.load(image_paths['laser']).convert_alpha()
+
+# (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+# Will pos the plane at the center of the screen/window
+player_rect = player_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+meteor_rect = meteor_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+laser_rect = laser_surf.get_frect(bottomleft=(20, WINDOW_HEIGHT - 10))
+
+# start
+star_surf = pygame.image.load(image_paths['star']).convert_alpha()
+# star pos
+star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
+
+# 1. -----  move right to left loop  ---
+# player_direction = 1
+#🤚 VECTOR
+player_direction = pygame.math.Vector2(20, -10)
+#20 X, - 10Y axis
+# print(player_direction)
+# -----  move right to left loop  ---
+
+while running:
+
+
+    clock.tick(60) #✋
+
+
+    for event in pygame.event.get():
+       if event.type == pygame.QUIT:
+            running = False
+
+    display_surface.fill("lavenderblush2")
+
+
+    for pos in star_positions:
+        display_surface.blit(star_surf, pos)
+
+
+   # player
+    display_surface.blit(player_surf, player_rect)
+
+    # meteor
+    display_surface.blit(meteor_surf, meteor_rect)
+    # laser
+    display_surface.blit(laser_surf, laser_rect)
+
+
+    # it will be multiplied by 100px every single frame
+    # player_rect.x += 20
+    #✋
+    #  if player x direction positive +=, then it will be increased +, hence will move to the right.
+    # -------------
+    # player_rect.y -= 10
+    # UP or down anim, depending of the values
+
+    # player_rect.center += (20, -10) # this will give you an error
+
+    # vector
+    player_rect.center += player_direction
+
+    pygame.display.update()
+
+
+
+pygame.quit()
